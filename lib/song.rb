58afba021885c4d6ff3c609036cdaee568d7e289
Marsha Annon <marshaannon@gmail.com>
2020-03-17 03:27:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,6 @@ class Song
   def initialize(name)
     @name = name
     @@all << self
-
   end
 
   def self.new_by_filename(file)
@@ -27,6 +26,5 @@ class Song
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
   end
-
-
+  
 end
