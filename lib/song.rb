b0c225ed25a960b8f0224a425177aadf3486eefa
Marsha Annon <marshaannon@gmail.com>
2020-03-17 03:25:00 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,26 @@
+require "pry"
+
+class Song
+
+  @@all = []
+
+  attr_accessor :name, :artist
+
+  def initialize(name)
+    @name = name
+    @@all << self
+
+  end
+
+  def self.new_by_filename(file)
+    song_info = file.chomp(".mp3").split(" - ")
+    song = Song.new(song_info[1])
+    @artist = song_info[0]
+    song
+  end
+
+  def self.all
+    @@all
+  end
+
+end
